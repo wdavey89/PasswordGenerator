0713f73bd7b09005ed4dd4fe2a06bcdd81c026ee
Wayne Davey <waynedavey89@hotmail.com>
2020-04-10 11:49:35 +0100

    Initial Commit

---
+++ PasswordGenerator/GeneratePassword.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class GeneratePassword
+    {
+        public static string Generate(string letters)
+        {
+            Console.Write("Enter a password length: ");
+            try
+            {
+                int passwordLength = Convert.ToInt32(Console.ReadLine()); // Convert user input to integer value
+                Random rnd = new Random();
+                char[] chars = new char[passwordLength]; // Create char array to the length of what the user inputted.
+                for (int i = 0; i < passwordLength; i++)
+                {
+                    chars[i] = letters[rnd.Next(letters.Length)];
+                }
+
+                return new string(chars);
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                return e.ToString();
+            }
+
+
+        }
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "PasswordGenerator.exe";
+            string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!£$%^&*()_<>?";
+            var password = GeneratePassword.Generate(letters);
+            Console.WriteLine(password);
+        }
+    }
+}
